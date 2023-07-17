39db8b22d1def6c037a984a171ca347fc16d897d
Evan Freeze <evanfreeze@gmail.com>
2023-07-16 20:15:39 -0500

    use default .medium and .large presentation detents instead of custom 1/3 one

--- ReadRate/Views/Now Reading Screen/NowReadingList.swift
+++ ReadRate/Views/Now Reading Screen/NowReadingList.swift
@@ -99,7 +99,7 @@ struct NowReadingList: View {
         }
         .sheet(isPresented: $showSheet, onDismiss: { bookStore.setTodaysTargets() }) {
             AddBook(bookStore: bookStore)
-                .presentationDetents([.fraction(0.33)])
+                .presentationDetents([.medium, .large])
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
             bookStore.setTodaysTargets()
