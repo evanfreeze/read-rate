8dcb337b934745df9e41c291a704b1910585078d
Evan Freeze <evanfreeze@gmail.com>
2023-07-16 20:56:59 -0500

    remove presentation detents for adding book since manual addition isn't a new sheet

--- ReadRate/Views/Now Reading Screen/NowReadingList.swift
+++ ReadRate/Views/Now Reading Screen/NowReadingList.swift
@@ -79,7 +79,6 @@ struct NowReadingList: View {
         }
         .sheet(isPresented: $showSheet, onDismiss: { bookStore.setTodaysTargets() }) {
             AddBook(bookStore: bookStore)
-                .presentationDetents([.medium, .large])
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
             bookStore.setTodaysTargets()
