829509423d5440910d822f95c0a99d4738daa655
Evan Freeze <evanfreeze@gmail.com>
2023-07-16 20:36:45 -0500

    use built-in AsyncImage instead of custom logic

--- ReadRate/Views/Shared Views/WebImage.swift
+++ ReadRate/Views/Shared Views/WebImage.swift
@@ -9,62 +9,20 @@
 import SwiftUI
 
 struct WebImage: View {
-    private enum LoadingState {
-        case loading, succeeded, failed
-    }
-    
-    private class ImageLoader: ObservableObject {
-        var data = Data()
-        var state: LoadingState = .loading
-        
-        init(url: String) {
-            guard let parsedUrl = URL(string: url) else {
-                self.state = .failed
-                return
-            }
-            
-            URLSession.shared.dataTask(with: parsedUrl) { data, response, error in
-                if let data = data, data.count > 0 {
-                    self.data = data
-                    self.state = .succeeded
-                } else {
-                    self.state = .failed
-                }
-                
-                DispatchQueue.main.async {
-                    self.objectWillChange.send()
-                }
-            }.resume()
-        }
-    }
-    
-    @StateObject private var loader: ImageLoader
-    var failureImage: Image
-    
-    init(url: String, failureImage: Image = Image(systemName: "text.book.closed")) {
-        _loader = StateObject(wrappedValue: ImageLoader(url: url))
-        self.failureImage = failureImage
-    }
+    let url: String
     
     var body: some View {
-        Group {
-            switch loader.state {
-            case .loading:
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                
-            case .failed:
-                failureImage
-                    .resizable()
-                    .foregroundColor(Color("PlaceholderBook"))
-            default:
-                if let image = UIImage(data: loader.data) {
-                    Image(uiImage: image).resizable()
-                } else {
-                    failureImage
+        AsyncImage(url: URL(string: url)) { phase in
+            switch phase {
+                case .failure:
+                    Image(systemName: "text.book.closed")
                         .resizable()
                         .foregroundColor(Color("PlaceholderBook"))
-                }
+                case .success(let image):
+                    image
+                        .resizable()
+                default:
+                    ProgressView()
             }
         }
     }
