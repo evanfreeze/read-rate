c5167d7deb3c9134710b5660f23af2ed8d2d1320
Evan Freeze <evanfreeze@gmail.com>
2024-02-06 18:40:56 -0600

    Cache book covers to avoid reloading them every time you view a book's details
    Previously, a book cover would be reloaded every time you went to view the book's details. Now, the images are cached so you should only see the image load the first time you view the book's details.

--- ReadRate/Views/Shared Views/WebImage.swift
+++ ReadRate/Views/Shared Views/WebImage.swift
@@ -9,27 +9,66 @@
 import SwiftUI
 
 struct WebImage: View {
-    let url: String
-    
-    var body: some View {
-        AsyncImage(url: URL(string: url)) { phase in
-            switch phase {
-                case .failure:
-                    Image(systemName: "text.book.closed")
-                        .resizable()
-                        .foregroundStyle(Color("PlaceholderBook"))
-                case .success(let image):
-                    image
-                        .resizable()
-                default:
-                    Image(systemName: "text.book.closed")
-                        .resizable()
-                        .foregroundStyle(Color("PlaceholderBook"))
-            }
-        }
-    }
-}
+     private enum LoadingState {
+         case loading, succeeded, failed
+     }
 
+     private class ImageLoader: ObservableObject {
+         var data = Data()
+         var state: LoadingState = .loading
+         
+         init(url: String) {
+             guard let parsedUrl = URL(string: url) else {
+                 self.state = .failed
+                 return
+             }
+
+             URLSession.shared.dataTask(with: parsedUrl) { data, response, error in
+                 if let data = data, data.count > 0 {
+                     self.data = data
+                     self.state = .succeeded
+                 } else {
+                     self.state = .failed
+                 }
+
+                 DispatchQueue.main.async {
+                     self.objectWillChange.send()
+                 }
+             }.resume()
+         }
+     }
+
+     @StateObject private var loader: ImageLoader
+     var failureImage: Image
+
+     init(url: String, failureImage: Image = Image(systemName: "text.book.closed")) {
+         _loader = StateObject(wrappedValue: ImageLoader(url: url))
+         self.failureImage = failureImage
+     }
+     
+     var body: some View {
+         Group {
+             switch loader.state {
+             case .loading:
+                 ProgressView()
+                     .progressViewStyle(CircularProgressViewStyle())
+
+             case .failed:
+                 failureImage
+                     .resizable()
+                     .foregroundColor(Color("PlaceholderBook"))
+             default:
+                 if let image = UIImage(data: loader.data) {
+                     Image(uiImage: image).resizable()
+                 } else {
+                     failureImage
+                         .resizable()
+                         .foregroundColor(Color("PlaceholderBook"))
+                 }
+             }
+         }
+     }
+}
 struct WebImage_Previews: PreviewProvider {
     static var previews: some View {
         WebImage(url: "https://daringfireball.net/graphics/logos/")
